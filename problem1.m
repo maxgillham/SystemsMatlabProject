19d9bb5db97754cd8216f5cca8fe7e2b16249e84
Max Gillham <14mfg2@queensu.ca>
2018-03-29 16:08:54 -0400

    Updated with part 2

--- problem1.m
+++ problem1.m
@@ -1,4 +1,5 @@
 
+%part i) Plot signal and distorted signal
 T = 0:.01:5;
 
 figure 
@@ -6,7 +7,29 @@ hold on
 
 plot(T,x(T));
 plot(T,y(x(T),n(T)));
-legend('x(t)','y(t)')
+legend('x(t)','y(t)');
+
+%part ii) plot CCFT of y, filter CCFT, plot inverse of filted transform
+
+y_sample = y(x(T), n(T));
+
+y_transformed = fft(y_sample);
+
+y_filtered = y_transformed;
+percent_cut = .027
+y_filtered(length(y_filtered)*percent_cut:length(y_filtered)*(1-percent_cut)) = 0;
+y_recover = ifft(y_filtered)
+
+figure
+hold on 
+plot(T, y_transformed);
+plot(T, y_filtered);
+legend('Forier Tansform of y(t)','Filtered Forier Transform of y(t)')
+figure 
+hold on
+plot(T, abs(y_recover), 'r');
+plot(T, x(T));
+legend('Recovered signal x(t)','Original signal x(t)')
 
 
 function x1 = x(T)
@@ -20,5 +43,3 @@ end
 function y1 = y(x,n)
     y1 = x + n;
 end
-
-
