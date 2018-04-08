ddaa68f248215ef8a2f014a0f42b17209e82bf7f
Max Gillham <14mfg2@queensu.ca>
2018-04-08 19:47:41 -0400

    got the butter filter working, all good now, just want to make graphs pretty

--- problem1.m
+++ problem1.m
@@ -1,6 +1,6 @@
 
 %part i) Plot signal and distorted signal
-T = 0:.1:40;
+T = linspace(0,10,81);
 
 figure 
 hold on
@@ -19,16 +19,6 @@ percent_cut = .0625;
 y_filtered(length(y_filtered)*percent_cut:length(y_filtered)*(1-percent_cut)) = 0;
 y_recover = ifft(y_filtered);
 
-%{
-ft1 = fft(x(T))
-ft2 = fft(n(T))
-nyquist(ft1)
-figure 
-hold on
-plot(T, ft1)
-plot(T, ft2)
-legend('forier x','forier n')
-%}
 
 figure
 hold on 
@@ -37,47 +27,24 @@ plot(T, y_filtered);
 legend('Forier Tansform of y(t)','Filtered Forier Transform of y(t)')
 figure 
 hold on
-plot(T, abs(y_recover), 'r');
+plot(T, real(y_recover), 'r');
 plot(T, x(T));
-legend('Recovered signal x(t)','Original signal x(t)')
+legend('Recovered signal','Original signal')
 
-%part iii) recover using butterword
+%part iii) recover using butterworth
 
-%{
-[A,B] = butter(1,.05,'low');
 
-[A,B] = butter(1,0.0625, 'low')
+[A,B,C,D] = butter(1,.29,'low');
+hd = ss2sos(A,B,C,D)
 
-y_butfilt = filter(A,B,y_transformed);
+y_butfiltr = sosfilt(hd,y_sample);
 
-y_butreco = ifft(y_butfilt);
-%}
-
-Wp = 45/200;
-Ws = 1/200;
-
-[p,W] = buttord(Wp,Ws,8,40)
-
-[A,B,C,D] = butter(p,W);
-hd = dfilt.statespace(A,B,C,D)
-y_fil = y_transformed;
-y_fil(length(y_fil)*percent_cut:length(y_fil)*(1-percent_cut)) = 0;
-
-y_butfilt = filter(hd,(y_fil));
-y_butreco = ifft(y_butfilt);
 
 figure
 hold on
-plot(T,abs(y_butreco));
+plot(T,(y_butfiltr));
 plot(T, x(T));
-legend('recovered, well sorta','orignal bish')
-figure 
-hold on
-plot(T, y_transformed)
-plot(T, y_butfilt)
-plot(T, y_filtered)
-legend('orignal trans', 'but hurt', 'ghetto filler')
-
+legend('Recovered Signal','Origonal Signal')
 
 function x1 = x(T)
     x1 = 10*exp((-T.^2)/2);
