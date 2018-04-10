b1b82efb1f774ea4486d44377990e5b3702d7627
Max Gillham <14mfg2@queensu.ca>
2018-04-10 11:41:37 -0400

    FINAL COPY FOR Q1

--- problem1.m
+++ problem1.m
@@ -1,7 +1,8 @@
-
-%part i) Plot signal and distorted signal
-T = linspace(0,10,81);
-y_sample = y(x(T), n(T));
+%----------------------------%
+%Part I) Plot signals x and y% 
+%----------------------------%
+T = linspace(0,10,81);       %since noise has frequency of 4Hz
+y_sample = y(x(T), n(T));    %use sampling rate of 81 from 0 to 10
 
 figure
 hold on
@@ -11,63 +12,61 @@ title('Part 1: Signal and Distored Signal','fontweight','bold','fontsize',16)
 legend('Origonal Signal','Distorted Signal');
 
 
-figure 
-hold on
-plot(T,fft(x(T)))
-plot(T,fft(n(T)))
-legend('x', 'n')
-%part ii) Recover signal using a low pass filter
-y_transformed = fft(y_sample);
-percent_cut = 0.075;
-y_filtered = y_transformed;
-y_filtered(length(y_filtered)*percent_cut:length(y_filtered)*(1-percent_cut)) = 0;
-y_recover = ifft(y_filtered);
+%-----------------------------------------------%
+%Part II) Recover signal using a low pass filter%
+%-----------------------------------------------%
+y_transformed = fft(y_sample);                  %taking forier transform of the distorted signal
+percent_retained = 0.075;                       %percent of frequency to be retained
+y_filtered = y_transformed;                     %setting forier segments from noise equal to zero
+y_filtered(length(y_filtered)*percent_retained:length(y_filtered)*(1-percent_retained)) = 0;
+y_recover = ifft(y_filtered);                   %getting back denoised signal
 
 
-figure
-hold on
-subplot(2,1,1);
-plot(T, y_transformed);
-title('Forier Transform of Distorted Signal','fontweight','bold','fontsize',16);
-subplot(2,1,2);
-plot(T, y_filtered);
-title('Filtered Forier Transform','fontweight','bold','fontsize',16)
-
-
+%---------------------------------------------------%
+%Part III) Plot recovered signal and origonal signal%
+%---------------------------------------------------%
 figure 
 hold on
 subplot(2,1,1);
 plot(T, real(y_recover), 'r');
-title('Recovered Signal','fontweight','bold','fontsize',16)
+title('Recovered Signal From Transform','fontweight','bold','fontsize',16)
 subplot(2,1,2);
 plot(T, x(T));
 title('Origonal Signal','fontweight','bold','fontsize',16)
 
 
-%part iii) recover using butterworth
+%------------------------------------%
+%Part IV) Recover x using butterworth%
+%------------------------------------%
+[A,B,C,D] = butter(1,.29,'low');     %configuring an ideal low pass 
+filt = ss2sos(A,B,C,D);              %filter for distorted signal
+y_butfiltr = sosfilt(filt,y_sample); %applying filter to sample 
 
 
-[A,B,C,D] = butter(1,.29,'low');
-hd = ss2sos(A,B,C,D)
-
-y_butfiltr = sosfilt(hd,y_sample);
-
-
+%-------------------------------------------------%
+%Part 5) Plot recovered signal and origonal signal%
+%-------------------------------------------------%
 figure
 hold on
+subplot(2,1,1);
 plot(T,(y_butfiltr));
+title('Recovered Signal from Ideal Filter','fontweight','bold','fontsize',16);
+subplot(2,1,2);
 plot(T, x(T));
-legend('Recovered Signal','Origonal Signal')
+title('Origonal Signal','fontweight','bold','fontsize',16);
 
 
-function x1 = x(T)
+%--------------------%
+%---FUNCTIONS USED---%
+%--------------------%
+function x1 = x(T)   %function for x(t)
     x1 = 10*exp((-T.^2)/2);
 end
 
-function n1 = n(T)
+function n1 = n(T)   %function for n(t)
     n1 = 20*cos(8*pi*T) + 2*sin(8*pi*T);
 end
 
-function y1 = y(x,n)
+function y1 = y(x,n) %function for y(x,n)
     y1 = x + n;
 end
